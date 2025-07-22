803515b9357ef86d3ecddd078f2fcc299f7c1ca9
Muhoti <strongmuhoti@gmail.com>
2025-07-22 17:47:21 +0300

    Update appVersion in Login.dart to 4.0.0 for the latest release, ensuring alignment with the current versioning strategy.

--- lib/Pages/Login.dart
+++ lib/Pages/Login.dart
@@ -538,7 +538,7 @@ Future<Message> login(String phone, String password) async {
       body: jsonEncode(<String, String>{
         'Phone': phone,
         'Password': password,
-        'appVersion': '3.0.0'
+        'appVersion': '4.0.0'
       }),
     );
 
